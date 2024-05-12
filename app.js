68e40e9f25d59ecfc9c234a62a799fa92209ad29
Oleh Hubskiy <gubskyolegod@gmail.com>
2024-05-12 20:11:14 +0300

    connect mongoDB

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const app = express();
+
+const logger = require("morgan");
+const cors = require("cors");
+require("dotenv").config();
+
 const eventsRouter = require("./routes/api/events");
+const PORT = process.env.PORT || 5001;
 
-const PORT = 5000;
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
+app.use(logger(formatsLogger));
+app.use(cors());
 app.use(express.json());
+
 app.use("/api/events", eventsRouter);
 
-app.listen(PORT, () => {
-  console.log(`Server listen on the port ${PORT}`);
+app.use((err, req, res, next) => {
+  const { status = 500, message = "Server found" } = err;
+  res.status(status).json({ message });
 });
+
+// app.listen(PORT, () => {
+//   console.log(`Server listen on the port ${PORT}`);
+// });
+
+module.exports = app;
